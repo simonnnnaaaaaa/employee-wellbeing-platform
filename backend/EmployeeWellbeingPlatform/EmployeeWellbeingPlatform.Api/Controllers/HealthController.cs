dd64a49f06acfd6be3896992a0bfaa1a3332d850
Simona Boicu <Simona.Boicu@endava.com>
2026-04-30 11:43:39 +0300

    Setup base API and health check endpoint

---
+++ backend/EmployeeWellbeingPlatform/EmployeeWellbeingPlatform.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWellbeingPlatform.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("API is working");
+    }
+}
